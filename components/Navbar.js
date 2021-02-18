6031c8098fbc853b7c0de8c8d2d7db6528ec0dd3
Monika <moniika.chmielewska@gmail.com>
2021-02-18 19:13:55 +0100

    Inital commit, working on the homepage sections

---
+++ components/Navbar.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <Image src="/logo.svg" alt="site logo" width={205} height={50} />
+      </div>
+      <div className="menu-links">
+      <Link href="/">
+        <a>home</a>
+      </Link>
+      <Link href="/about">
+        <a>about</a>
+      </Link>
+      <Link href="/projects">
+        <a>projects</a>
+      </Link>
+      <Link href="/contact">
+        <a>contact</a>
+      </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat iaculis felis eu pulvinar. Nunc eu lectus at eros varius tempus. Aenean viverra ut urna vitae vestibulum. Nullam eu sem eros. Integer vehicula felis at hendrerit vulputate. Integer semper lobortis metus, in faucibus nisi egestas ac. In ac laoreet quam, non sagittis mauris. Cras pharetra nulla lectus, id accumsan velit consectetur sollicitudin. Nunc vitae velit eu nisl blandit pellentesque. Vivamus a tortor at erat condimentum fringilla sed in diam. </p>
+        </div>
+    );
+}
+ 
+export default About;
