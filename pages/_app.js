488e5bb6d14d5d02813df812c1b9e7fc8aea02e6
Monika <moniika.chmielewska@gmail.com>
2021-03-01 18:37:16 +0100

    Progress bar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import Layout from '../components/Layout';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import '../styles/globals.scss';
+
+
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
