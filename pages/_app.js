cc4039b49b15df124094e5988c593c8af30ba3a9
Monika <moniika.chmielewska@gmail.com>
2021-03-02 10:36:28 +0100

    Responsiveness fixes

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 import '../styles/globals.scss';
 
-
 Router.events.on('routeChangeStart', () => NProgress.start()); 
 Router.events.on('routeChangeComplete', () => NProgress.done()); 
 Router.events.on('routeChangeError', () => NProgress.done());
